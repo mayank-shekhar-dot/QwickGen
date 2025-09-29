80ec80b5d943642d794881e6479a95917c63ba34
mayank-shekhar-dot <mayankartist2010@gmail.com>
2025-09-29 11:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, request, jsonify, send_from_directory render_template
+from flask import Flask, request, jsonify, send_from_directory, render_template
 from flask_cors import CORS
 import together
 import base64
@@ -235,6 +235,7 @@ def health_check():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
