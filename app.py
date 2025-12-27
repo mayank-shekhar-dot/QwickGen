64ba92153d8c6bc9c9bd934a3f37334c4c6b6abd
mayank-shekhar-dot <mayankartist2010@gmail.com>
2025-12-27 10:13:23 +0530

    Update app.py
    Signed-off-by: mayank-shekhar-dot <mayankartist2010@gmail.com>

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import requests
 
 @app.before_request
 def force_www():
-if request.host == "quickgenai.in":
-return redirect("https://www.quickgenai.in" + request.full_path, code=301)
+    if request.host == "quickgenai.in":
+        return redirect(
+            "https://www.quickgenai.in" + request.full_path,
+            code=301
+        )
 
 # ----------------------------
 # Configure logging
@@ -235,6 +238,7 @@ def health_check():
 # ----------------------------
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
